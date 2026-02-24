b432c0791128c2904cf31e640dc0e9e1482f867b
mohammadeliasy <mohammad@chappack>
2026-02-24 14:53:01 +0330

    export service repo

--- services/core/cmd/main.go
+++ services/core/cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main(){
-	
+import "github.com/gin-gonic/gin"
+
+func main() {
+	route := gin.Default()
+
+	route.GET("/",func(c *gin.Context) {
+		c.JSON(200,gin.H{
+			"message": "this is ok",
+		})
+	})
+	route.Run(":8000")
 }
